9a3c9cd64667efedfd680300b7e8f7ab0177dd26
PilarGuataquira <j.guataquira@uniandes.edu.co>
2022-09-17 19:15:17 -0500

    Se crean módulos y entidades para aeropuerto y aerolinea

---
+++ src/aerolinea/aerolinea.entity.ts
@@ -0,0 +1,26 @@
+import { AeropuertoEntity } from '../aeropuerto/aeropuerto.entity';
+import { Column, Entity, JoinTable, ManyToMany, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class AerolineaEntity {
+ @PrimaryGeneratedColumn('uuid')
+ id: string;
+
+ @Column()
+ nombre: string;
+ 
+ @Column()
+ descripcion: string;
+ 
+ @Column()
+ fechafundacion: Date;
+ 
+ @Column()
+ paginaweb: string;
+ 
+ @ManyToMany(() => AeropuertoEntity, aeropuerto => aeropuerto.aerolineas)
+ @JoinTable()
+   aeropuertos: AeropuertoEntity[];
+
+}
+
