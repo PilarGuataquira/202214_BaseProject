c7d2f4a9e3edd2998b885861ac7d29b4166bfbf2
PilarGuataquira <j.guataquira@uniandes.edu.co>
2022-10-30 07:53:41 -0500

    Primera prueba sonar

--- src/aerolinea/aerolinea.module.ts
+++ src/aerolinea/aerolinea.module.ts
@@ -5,8 +5,10 @@ import { AerolineaService } from './aerolinea.service';
 import { AerolineaController } from './aerolinea.controller';
 
 @Module({
+
   imports: [TypeOrmModule.forFeature([AerolineaEntity])],
   providers: [AerolineaService],
   controllers: [AerolineaController]
+  
 })
 export class AerolineaModule {}
